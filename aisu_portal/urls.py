2ba67760f83ce907b8ddea1cebc893e0300c1189
Babur Rayan <rayanbabur@outlook.com>
2026-02-05 20:16:58 +0530

    homepage redirect added

--- aisu_portal/urls.py
+++ aisu_portal/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect   # ADD THIS
+
+from members import views               # ADD THIS if not present
 
 urlpatterns = [
+    path('', lambda request: redirect('/admin/')),  # homepage
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+
+    path('super-admin/', views.super_admin_dashboard, name='super_admin'),
+    path('it/', views.it_dashboard, name='it_dashboard'),
+    path('state/', views.state_dashboard, name='state_dashboard'),
+    path('district/', views.district_dashboard, name='district_dashboard'),
 ]
