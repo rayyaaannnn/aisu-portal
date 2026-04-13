23c0aeee87906428b8ac469a1fed31db2c6457f1
Babur Rayan <rayanbabur@outlook.com>
2026-04-13 12:00:47 +0530

    Update and rename wsgi.py to passenger_wsgi.py

---
+++ aisu_portal/passenger_wsgi.py
@@ -0,0 +1,8 @@
+import sys
+import os
+INTERP = os.path.expanduser("~/virtualenv/aisuin/3.10/bin/python")
+if sys executable != INTERP:
+os.excel(INTERP, INTERP, *sys.argv)
+sys.path.insert(0, os.path.dirname(__file__))
+
+from app import app as application
